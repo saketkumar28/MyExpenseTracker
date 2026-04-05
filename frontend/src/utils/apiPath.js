497b827c459bf023c19a368125cab68899b91ae5
saketkumar28 <kumarsaket2801@gmail.com>
2026-04-06 00:06:08 +0530

    Final push

--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -1,30 +1,29 @@
 // import { downloadIncomeExcel } from "../../../backend/controllers/incomeController";
 
-export const BASE_URL = 'http://localhost:8000/api/v1';
+export const BASE_URL = "https://myexpensetracker-52ah.onrender.com";
 
 export const API_PATH = {
-    AUTH: {
-      LOGIN : "/auth/login",
-      REGISTER: "/auth/register",
-      GET_USER_INFO: "/auth/getUserInfo", // Updated to match backend route
-    },
-    DASHBOARD: {
-      GET_DASHBOARD_DATA: "/dashboard",
-    },
-    INCOME : {
-      ADD_INCOME: "/income/add",
-      GET_ALL_INCOME: "/income/getAll",
-      DELETE_INCOME: (incomeId) => `/income/${incomeId}`,
-      DOWNLOAD: "/income/download-excel", // ✅ use the imported function
-    },
-    EXPENSE: {
-      ADD_EXPENSE: "/expense/add",
-      GET_ALL_EXPENSE: "/expense/getAll",
-      DELETE_EXPENSE: (expenseId) => `/expense/${expenseId}`,
-      DOWNLOAD: `/expense/download-excel`,
-    },
-    IMAGE : {
-      UPLOAD_IMAGE: "/auth/upload-image"
-    },
-    
-  }
+  AUTH: {
+    LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
+    GET_USER_INFO: "/auth/getUserInfo", // Updated to match backend route
+  },
+  DASHBOARD: {
+    GET_DASHBOARD_DATA: "/dashboard",
+  },
+  INCOME: {
+    ADD_INCOME: "/income/add",
+    GET_ALL_INCOME: "/income/getAll",
+    DELETE_INCOME: (incomeId) => `/income/${incomeId}`,
+    DOWNLOAD: "/income/download-excel", // ✅ use the imported function
+  },
+  EXPENSE: {
+    ADD_EXPENSE: "/expense/add",
+    GET_ALL_EXPENSE: "/expense/getAll",
+    DELETE_EXPENSE: (expenseId) => `/expense/${expenseId}`,
+    DOWNLOAD: `/expense/download-excel`,
+  },
+  IMAGE: {
+    UPLOAD_IMAGE: "/auth/upload-image",
+  },
+};
