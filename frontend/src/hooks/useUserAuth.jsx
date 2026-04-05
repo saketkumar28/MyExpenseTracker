c4217083fc421cab1eeaf5d5851e4173668e1a21
saketkumar28 <kumarsaket2801@gmail.com>
2026-04-05 23:31:56 +0530

    Correct

--- frontend/src/hooks/useUserAuth.jsx
+++ frontend/src/hooks/useUserAuth.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../context/userContext.jsx";
+import { UserContext } from "../context/UserContext.jsx";
 import { axiosInstance } from "../utils/axiosInstance.js";
 import { API_PATH } from "../utils/apiPath.js";
 
